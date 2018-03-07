7097f24221f4f1489fe906cd8a3654be81b85885
Benjamin P. McKenna <britishben@gmail.com>
2018-03-07 18:06:45 +0000

    test forwards deps

--- aptimise.sh
+++ aptimise.sh
@@ -6,18 +6,30 @@ set -euo pipefail #safety line
 
 VERSION=0.1.0
 
+#installed=$(dpkg-query -W -f '${Package}\t${Status}\n' | awk '$NF=="installed"{print $1}')
 installed=${*:-$(apt-mark showmanual)}
 #installed=$(apt list --manually-installed)
 
+declare -A list
+
 for i in $installed; do
-    for j in $installed; do
-        unset found;
-        echo -n .
-        if $(apt-cache rdepends -i $j | grep -q "^\s\+$i$" ); then
-            found=$j;
-            #printf '\nfound %s in %s\n' $i $j
-            break
+    list["$i"]=0;
+done
+
+for i in $installed; do
+    if [ ${list["$i"]} > 0 ]; then continue; fi
+    deps=$(apt-cache depends -i --implicit --recurse "$i" | grep -oP "Depends:\\s<?\K.*") || true
+
+    for j in $deps; do
+        #echo "  $i:$j"
+        if [ "${list["$j"]:-"false"}" ]; then
+            list["$i"]++;
+            continue;
         fi
     done
-    echo \n${found:-$i}
+done
+
+for i in $installed; do
+    if ${list["$i"]} == "true"; then echo $i; fi
+    printf '\t%s: %s\n' $i ${list["$i"]}
 done
